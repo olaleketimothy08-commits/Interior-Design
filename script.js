2c7d1b6c59c999765b7de0b650006cd5d8737628
olaleketimothy08 <olaleketimothy08@gmail.com>
2025-11-21 10:03:50 +0000

    chhanged

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function validateForm() {
+  let name = document.getElementById("fname").value;
+  let subject = document.getElementById("subject").value;
+  let email = document.getElementById("email").value;
+  let number = document.getElementById("number").value;
+let errorMessage = document.getElementById("errorMessage");
+
+  errorMessage.innerHTML = "";
+  document.getElementById('errorMessage').style.color = "red"
+
+  if (name.length <= 5) {
+    errorMessage.innerHTML = "Name must be more than five characters long.";
+    return false;
+  }
+
+  if (subject.length <= 1  ){
+        errorMessage.innerHTML = "pelease write your code"
+    return false;
+  }
+
+   if (!email.includes ('.') || !email.includes ('@')  ){
+        errorMessage.innerHTML = "pelease enter a valid email address e.g example@gmail.com";
+        return false;
+  }
+
+  if (number.length != 11) {
+    errorMessage.innerHTML = "Please enter a valid phone number";
+    return false;
+  }
+}
